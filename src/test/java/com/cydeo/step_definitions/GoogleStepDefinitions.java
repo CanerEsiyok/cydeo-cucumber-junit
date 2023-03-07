78b8e024bf35e5d4ef72c723ba1e87c4d255ab9a
Caner Esiyok <esiyokcaner@gmail.com>
2023-03-07 18:32:23 +0100

    junit report added2

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -13,9 +13,10 @@ public class GoogleStepDefinitions {
     public void user_is_on_google_search_page() {
         Driver.getDriver().get("https://www.google.com");
 
-       // googleSearchPage.cookies.click();
+        googleSearchPage.cookies.click();
 
     }
+
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
         String expectedTitle="Google";
